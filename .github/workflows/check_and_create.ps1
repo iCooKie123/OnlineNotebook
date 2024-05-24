7e72f06135c4dafe32ffa30f442fffba0421eb1a
Alex <aconstantin86@yahoo.com>
2024-05-24 18:32:07 +0300

    Add new flow

--- .github/workflows/check_and_create.ps1
+++
@@ -1,9 +0,0 @@
-
-param (
-    [string]$specPath
-)
-
-if (-Not (Test-Path $specPath)) {
-    New-Item -ItemType Directory -Path (Split-Path $specPath) -Force
-    New-Item -ItemType File -Path $specPath -Force
-}
